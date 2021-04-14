9e5e463f163b620cbbfcd598e727ba52f1a53216
Mimirko <emirorellana18@gmail.com>
2021-04-14 20:28:12 -0300

    se cambio la direccion de la db

--- database.php
+++ database.php
@@ -1,9 +1,9 @@
 <?php
 
 $server = 'localhost';
-$username = '3627';
-$password = '3627';
-$database = '3627';
+$username = 'adm_webgenerator';
+$password = 'webgenerator2020';
+$database = 'webgenerator';
 
   $conn = mysqli_connect($server, $username, $password, $database);
 
